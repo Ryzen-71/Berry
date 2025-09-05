3623144d734708f7a3ff391b11cf872cabafd8e0
Max Mitrokhin <super.ya-prio237@ya.ru>
2025-09-06 00:01:09 +0300

    feat: добился максимального совпадения с макетом главнойстраницы (index.html) — вертикальная погрешность не более 10 пикселей, горизонтальная погрешность не более 5 пикселей.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
-/* в этот файл добавляет скрипты*/
+const headerNav = document.querySelector('.main-header__nav');
+const buttonBurger = document.querySelector('.main-header__toggler');
+
+headerNav.classList.remove('main-header__nav--nojs');
+
+
+buttonBurger.addEventListener('click', () => {
+  if(headerNav.classList.contains('main-header__nav--closed')) {
+    headerNav.classList.remove('main-header__nav--closed');
+    headerNav.classList.add('main-header__nav--opened');
+  } else {
+    headerNav.classList.remove('main-header__nav--opened');
+    headerNav.classList.add('main-header__nav--closed');
+
+  }
+}
+);
